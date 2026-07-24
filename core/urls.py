2befe562b4d429f125c50d9e5b2b1b24067e7f0f
henriquearaujo20141-rgb <henriquearaujo20141@gmail.com>
2026-07-24 16:32:03 -0300

    google

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,21 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import include, path
 from billing.views import StripeWebhookView
 
+
+def google_site_verification(request):
+    return HttpResponse(
+        'google-site-verification: google971862346ff0bb4b.html\n',
+        content_type='text/html; charset=utf-8',
+    )
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('google971862346ff0bb4b.html', google_site_verification, name='google_site_verification'),
     path('', include("home.urls")),
     path('users/', include("users.urls")),
     path('certidao/', include("certidao.urls")),
